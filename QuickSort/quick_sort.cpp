249df9118a585ca6d02fb69c3d149f348d3176a1
Nastya <olinan106@gmail.com>
2025-11-19 19:44:17 +0400

    добавила функцию mergeSort

--- QuickSort/quick_sort.cpp
+++ QuickSort/quick_sort.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-vector<int> quickSortSimple(vector<int> arr) {
+vector<int> quickSort(vector<int> arr) {
 
     if (arr.size() <= 1) {
         return arr;
@@ -21,12 +21,53 @@ vector<int> quickSortSimple(vector<int> arr) {
     }
 
     vector<int> result;
-    vector<int> sortedLess = quickSortSimple(less);
-    vector<int> sortedBig = quickSortSimple(big);
+    vector<int> sortedLess = quickSort(less);
+    vector<int> sortedBig = quickSort(big);
 
     result.insert(result.end(), sortedLess.begin(), sortedLess.end());
     result.push_back(pivot);
     result.insert(result.end(), sortedBig.begin(), sortedBig.end());
+
+    return result;
+}
+
+vector<int> mergeSort(vector<int> arr) {
+
+    if (arr.size() <= 1) {
+        return arr;
+    }
+
+    int mid = arr.size() / 2;
+
+    vector<int> left(arr.begin(), arr.begin() + mid);
+    vector<int> right(arr.begin() + mid, arr.end());
+
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    vector<int> result;
+    int i = 0, j = 0;
+
+    while (i < left.size() && j < right.size()) {
+        if (left[i] <= right[j]) {
+            result.push_back(left[i]);
+            i++;
+        }
+        else {
+            result.push_back(right[j]);
+            j++;
+        }
+    }
+
+    while (i < left.size()) {
+        result.push_back(left[i]);
+        i++;
+    }
+
+    while (j < right.size()) {
+        result.push_back(right[j]);
+        j++;
+    }
 
     return result;
 }
@@ -52,10 +93,13 @@ int main() {
     for (int num : arr) cout << num << " ";
     cout << endl;
 
-    vector<int> sorted = quickSortSimple(arr);
-
-    cout << "Отсортированный массив: ";
-    for (int num : sorted) cout << num << " ";
+    vector<int> quickSorted = quickSort(arr);
+    cout << "Быстрая сортировка: ";
+    for (int num : quickSorted) cout << num << " ";
     cout << endl;
 
+    vector<int> mergeSorted = mergeSort(arr);
+    cout << "Сортировка слиянием: ";
+    for (int num : mergeSorted) cout << num << " ";
+    cout << endl;
 }
